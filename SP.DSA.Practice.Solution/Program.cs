d7cad2f2f76d51ddb6512338449df461a224f89c
shubh <shubh@LAPTOP-SDS90F74>
2020-08-26 18:15:57 -0400

    Add project files.

---
+++ SP.DSA.Practice.Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SP.DSA.Practice.Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
